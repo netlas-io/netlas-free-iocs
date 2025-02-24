880d57b72c8a2e0941565767a57343eb35a5943d
Arthur Kotylevskiy <kotylevskiy@gmail.com>
2025-02-24 11:58:59 +0300

    More debuggin info added to script.sh

--- update.sh
+++ update.sh
@@ -3,19 +3,32 @@
 SSLBL_URL=$(yq '.sslbl_url' config.yaml)
 if [ -z "$SSLBL_URL" ]; then
     echo "Error: sslbl_url not found in config.yaml"
-    exit 1
+    exit 2
 fi
 
 DATABASE_BASE_URL=$(yq '.database_base_url' config.yaml)
 DATABASE_FILE=$(yq '.database_file' config.yaml)
 
+echo "SSLBL_URL: $SSLBL_URL"
+echo "DATABASE_BASE_URL: $DATABASE_BASE_URL"
+echo "DATABASE_FILE: $DATABASE_FILE"
+
 echo "Downloading the latest files from Netlas and Abuse.ch..."
 
 if [ -n "$DATABASE_FILE" ]; then
     wget -q --show-progress -O "$DATABASE_FILE" "$DATABASE_BASE_URL""$DATABASE_FILE"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to download $DATABASE_FILE from $DATABASE_BASE_URL"
+        exit 3
+    fi
     wget -q --show-progress -O "$DATABASE_FILE".md5 "$DATABASE_BASE_URL""$DATABASE_FILE".md5
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to download $DATABASE_FILE.md5 from $DATABASE_BASE_URL"
+        exit 4
+    fi
 else
     echo "Error: database_file not found in config.yaml"
+    exit 5
 fi
 
 # Exit immediately if a command fails
@@ -30,6 +43,10 @@ SSLBL_FILE="sslbl.csv"
 SSLBL_EXTENDED_FILE="sslbl_extended.json"
 
 wget -q --show-progress -O "$SSLBL_FILE" "$SSLBL_URL"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download $SSLBL_FILE from $SSLBL_URL"
+    exit 6
+fi
 
 echo "Running the update scripts..."
 
@@ -43,7 +60,7 @@ fi
 # Check if the first script exited successfully
 if [[ $? -ne 0 ]]; then
     echo "Error: $SCRIPT1 failed. Exiting."
-    exit 2
+    exit 7
 fi
 
 # Check if the output file exists after the first script completes successfully
@@ -55,17 +72,15 @@ if [[ -f "$NETLAS_OUTPUT_FILE" ]]; then
     # Check if the second script exited successfully
     if [[ $? -ne 0 ]]; then
         echo "Error: $SCRIPT2 failed. Exiting."
-        exit 3
+        exit 8
     fi
 
     # Delete the output file after the second script completes successfully
     rm "$NETLAS_OUTPUT_FILE"
 else
     echo "Output file $NETLAS_OUTPUT_FILE not found. Second script will not run."
-    exit 4
+    exit 9
 fi
-
-echo "Generating STATS.md..."
 
 # Run the third script to generate extended sslbl data
 python3 "$SCRIPT3" -i "$SSLBL_FILE" -o "$SSLBL_EXTENDED_FILE" -p 0
