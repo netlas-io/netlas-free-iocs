cd9ebbd3eaa0d636fca3105d547dea6a050e5786
Arthur Kotylevskiy <kotylevskiy@gmail.com>
2025-02-25 13:57:47 +0300

    Enhance threat analysis by adding relevance period and updating output tables to include relevant counts from SSLBL and Netlas data

--- stats.py
+++ stats.py
@@ -3,6 +3,8 @@ import csv
 import json
 from datetime import datetime
 
+RELEVANCE_PERIOD = 90
+
 def main():
     parser = argparse.ArgumentParser(description="Analyze Extended SSLBL JSON for top threats")
     parser.add_argument("extended_sslbl_json", type=str, help="Extended SSLBL JSON file to analyze (mandatory)")
@@ -32,12 +34,16 @@ def main():
         exit(1)
 
     # Load Netlas SSLBL Malicious Hosts CSV
-    netlas_threats_set = set()
+    netlas_threats = {}
     try:
         with open(args.netlas_sslbl_malicious_hosts_csv, 'r') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
-                netlas_threats_set.add(row['threat'])
+                if ((datetime.now() - datetime.strptime(row['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")).days <= RELEVANCE_PERIOD):
+                    if row['threat'] not in netlas_threats:
+                        netlas_threats[row['threat']] = 1
+                    else:
+                        netlas_threats[row['threat']] += 1
     except Exception as e:
         print(f"Error: {e}")
         exit(2)
@@ -67,10 +73,11 @@ def main():
             'last_seen': lseen,
             'certs_count': value['certs_count'],
             'seen_count': len(value['samples']),
+            'relevant_seen_count': len([sample for sample in value['samples'] if (datetime.now() - datetime.strptime(sample['timestamp'], "%Y-%m-%d %H:%M:%S")).days <= RELEVANCE_PERIOD]),
             'ports': top_ports
         })
 
-    # Sort the threats first by last_seen, then by seen_count
+    # Sort the threats first by last_seen
     sslbl_sorted_threats = sorted(sslbl_top_threats, key=lambda x: x['last_seen'], reverse=True)
 
     # Limit to the top -n threats
@@ -89,27 +96,38 @@ def main():
     # Generate Markdown output
     md_output = []
 
+    md_output.append(f"## TOP-{args.number_of_threats} Most Recent Threats from Abuse.ch SSLBL")
+    md_output.append(f"Date of report: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    md_output.append("\n**Columns Description**")
+    md_output.append("- **Threat**: The name or identifier of the threat")
+    md_output.append("- **Last Seen**: The most recent date and time the threat was observed in the Abuse.ch SSLBL database")
+    md_output.append("- **Certs**: The number of certificates associated with the threat in the Abuse.ch SSLBL database")
+    md_output.append("- **Top Ports**: The top ports used by the threat, limited to the top 5")
+    md_output.append(f"- **SSLBL Relevant**: The number of samples observed within the last {RELEVANCE_PERIOD} days in the Abuse.ch SSLBL database")
+    md_output.append(f"- **Netlas Data Relevant**: The number of entries observed within the last {RELEVANCE_PERIOD} days in the Netlas SSLBL Malicious Hosts database")
+    md_output.append("\n\n")
+
     # Header for the threats table
-    md_output.append("| Threat                     | Last Seen                | Certs   | Seen      | Top Ports                        | Feed |")
-    md_output.append("|----------------------------|--------------------------|---------|-----------|----------------------------------|------|")
+    md_output.append("| Threat                     | Last Seen           | Top Ports          | Certs        | SSLBL Relevant   | Netlas Data Relevant  |")
+    md_output.append("|----------------------------|---------------------|--------------------|-------------:|-----------------:|----------------------:|")
 
     # Add each threat to the table
     for threat in sslbl_top_n_threats:
         formatted_last_seen = threat['last_seen'].strftime("%Y-%m-%d %H:%M:%S") if threat['last_seen'] else "N/A"
         top_ports_list = list(threat['ports'].keys())[:5]
         top_ports_display = ', '.join(top_ports_list)
-        ioc_mark = "+" if threat['threat'] in netlas_threats_set else "-"
-        md_output.append(f"| {threat['threat']:<26} | {formatted_last_seen:<24} | {threat['certs_count']:<7} | {threat['seen_count']:<9} | {top_ports_display:<32} | {ioc_mark:<4} |")
+        ioc_count = netlas_threats.get(threat['threat'], 0)
+        md_output.append(f"| {threat['threat']:<26} | {formatted_last_seen} | {top_ports_display} | {threat['certs_count']} | {threat['relevant_seen_count']} | {ioc_count} |")
 
     # Header for the total ports count table
     md_output.append("\n## Total Ports Count across Top Threats")
     md_output.append("| Port       | Count      |")
-    md_output.append("|------------|------------|")
+    md_output.append("|------------|-----------:|")
 
     # Add each port count to the table
     sorted_total_ports = sorted(filtered_total_ports_count.items(), key=lambda item: item[1], reverse=True)
     for port, count in sorted_total_ports:
-        md_output.append(f"| {port:<10} | {count:<10} |")
+        md_output.append(f"| {port} | {count} |")
 
     # Convert the list to a single string
     md_output_str = "\n".join(md_output)
